a7bf00cb64c7f659ce47d60a405faad545d34945
Michael <michael@ITAD_platform.com>
2025-08-19 13:11:09 -0400

    reverted logger back to the more readable format, it apparently doesnt make a different on preformance

--- BuildManager/Logger/logger.go
+++ BuildManager/Logger/logger.go
@@ -12,34 +12,36 @@ import (
 
 type LogView struct {
 	widget.BaseWidget
-	lines []string
-	max   int
-	lineH float32
+	max     int
+	lineH   float32
+	objects []*canvas.Text
 }
 
 func NewLogView(maxLines int) *LogView {
 	l := &LogView{
-		lines: []string{},
-		max:   maxLines,
-		lineH: float32(theme.TextSize()) + 4, // spacing per line
+		objects: []*canvas.Text{},
+		max:     maxLines,
+		lineH:   float32(theme.TextSize()) + 4, // spacing per line
 	}
 	l.ExtendBaseWidget(l)
 	return l
 }
 
 func (l *LogView) Clear() {
-	l.lines = l.lines[:0]
+	l.objects = l.objects[:0]
 	l.Refresh()
 }
 
 // Append a new log line
 func (l *LogView) AppendLine(line string) {
 	line = strings.ReplaceAll(line, "\n", "")
-	if len(l.lines) >= l.max {
-		copy(l.lines, l.lines[1:])
-		l.lines[len(l.lines)-1] = line
+	txt := canvas.NewText(line, theme.Color(theme.ColorNameForeground))
+	txt.TextSize = theme.TextSize()
+	txt.TextStyle = fyne.TextStyle{Monospace: true}
+	if len(l.objects) >= l.max {
+		l.objects = append(l.objects[1:], txt)
 	} else {
-		l.lines = append(l.lines, line)
+		l.objects = append(l.objects, txt)
 	}
 	l.Refresh()
 }
@@ -58,43 +60,44 @@ type logRenderer struct {
 func (r *logRenderer) MinSize() fyne.Size {
 	// Width grows with longest line, height grows with number of lines
 	width := float32(200)
-	for _, line := range r.log.lines {
-		w := fyne.MeasureText(line, theme.TextSize(), fyne.TextStyle{}).Width
+	for _, line := range r.log.objects {
+		w := fyne.MeasureText(line.Text, theme.TextSize(), fyne.TextStyle{}).Width
 		if w > width {
 			width = w
 		}
 	}
-	height := float32(len(r.log.lines)) * r.log.lineH
+	height := float32(len(r.log.objects)) * r.log.lineH
 	return fyne.NewSize(width+8, height)
 }
 
 func (r *logRenderer) Layout(size fyne.Size) {
 	r.bg.Resize(size)
+
+	y := float32(0)
+	for _, obj := range r.log.objects {
+		obj.Move(fyne.NewPos(4, y))
+		y += r.log.lineH
+	}
 }
 
 func (r *logRenderer) Refresh() {
-	// Rebuild all objects: background + text lines
-	objects := []fyne.CanvasObject{r.bg}
-	y := float32(0)
-	for _, line := range r.log.lines {
-		txt := canvas.NewText(line, theme.Color(theme.ColorNameForeground))
-		txt.TextSize = theme.TextSize()
-		txt.TextStyle = fyne.TextStyle{Monospace: true}
-		txt.Move(fyne.NewPos(4, y))
-		y += r.log.lineH
-		objects = append(objects, txt)
-	}
 	r.bg.FillColor = theme.Color(theme.ColorNameBackground)
 	r.bg.Refresh()
+
+	for _, t := range r.log.objects {
+		t.Color = theme.Color(theme.ColorNameForeground)
+		t.Refresh()
+	}
 	canvas.Refresh(r.log)
-	// Replace renderer’s object list
-	rObjs := make([]fyne.CanvasObject, len(objects))
-	copy(rObjs, objects)
-	rObjects[r.log] = rObjs
 }
 
-var rObjects = make(map[*LogView][]fyne.CanvasObject)
-
 func (r *logRenderer) BackgroundColor() color.Color { return theme.Color(theme.ColorNameBackground) }
-func (r *logRenderer) Objects() []fyne.CanvasObject { return rObjects[r.log] }
-func (r *logRenderer) Destroy()                     {}
+func (r *logRenderer) Objects() []fyne.CanvasObject {
+	objs := make([]fyne.CanvasObject, 1+len(r.log.objects))
+	objs[0] = r.bg
+	for i, t := range r.log.objects {
+		objs[i+1] = t
+	}
+	return objs
+}
+func (r *logRenderer) Destroy() {}
